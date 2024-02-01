54ef697dfb65178771cdad557bfbf007bc63e0d4
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-02-01 11:18:28 +0000

    Complete EventDetails component

---
+++ src/components/EventDetails.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const EventDetails = () => {
+  return (
+    <div className = 'card'>
+      <div className = 'card-content'>
+        <div className = 'media'>
+          <div className = 'media-left'>
+            <figure className = 'image is-96x96'>
+              <img src="https://place-puppy.com/200x200" alt="Puppy" />
+            </figure>
+          </div>
+
+          <div className = 'media-content'>
+            <p className = 'title is-4'>Puppy petting party</p>
+            <p className = 'subtitle is-6'>Come and join in the fun</p>
+            <p>
+              That's right, just pop your details below and you'll be part of the puppy petting party!!
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default EventDetails
