1502b480a90fab78d988f9263e1ccf9491c3effc
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-02-01 14:03:37 +0000

    EventSignUpForm component complete

--- src/components/EventSignUpForm.jsx
+++ src/components/EventSignUpForm.jsx
@@ -39,10 +39,86 @@ const EventSignUpForm = ({ addEventAttendee }) => {
     setSignUpComplete(false);
   }
   return (
-    <div>
+    <>
+      <EventDetails />
+      <div className="box">
+          {
+              !signUpComplete && (
+                  <form onSubmit={handleFormSubmit}>
+                      <div className="field is-horizontal">
+                          <div className="field-label is-normal">
+                              <label className="label">From</label>
+                          </div>
+                          <div className="field-body">
+                              <div className="field">
+                                  <p className="control is-expanded">
+                                      <input className="input" type="text" placeholder="Name" id="name" value={formFields.name} onChange={handleOnChange}  />
+                                  </p>
+                              </div>
+                              <div className="field">
+                                  <p className="control is-expanded">
+                                      <input className="input" type="email" placeholder="Email" id="email" value={formFields.email} onChange={handleOnChange} />
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
 
-    </div>
+                      <div className="field is-horizontal">
+                          <div className="field-label"></div>
+                          <div className="field-body">
+                              <div className="field is-expanded">
+                                  <div className="field has-addons">
+                                      <p className="control">
+                                          <a className="button is-static" href="#nogo">
+                                              +49
+                                          </a>
+                                      </p>
+                                      <p className="control is-expanded">
+                                          <input className="input" type="tel" placeholder="Your phone number" id="number" value={formFields.number} onChange={handleOnChange} />
+                                      </p>
+                                  </div>
+                                  <p className="help">Do not enter the first zero</p>
+                              </div>
+                          </div>
+                      </div>
+
+                      <div className="field is-horizontal">
+                          <div className="field-label"></div>
+                          <div className="field-body">
+                              <div className="field">
+                                  <div className="control">
+                                      <button className="button is-primary">
+                                          Sign me up!
+                                      </button>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </form>
+              )
+          }
+          {
+              signUpComplete && (
+                  <>
+                      <p className="is-subtitle is-size-4 has-text-primary">
+                          Thanks for signing up! We'll see you soon
+                          <button className="button is-link is-pulled-right" onClick={resetForm}>Add another</button>
+                      </p>
+                  </>
+              )
+          }
+      </div>
+    </>
   )
 }
 
-export default EventSignUpForm
+const mapDispatchToProps = {
+  addEventAttendee
+}
+
+
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(EventSignUpForm);
