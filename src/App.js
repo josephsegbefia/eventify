2ccacccdba9259e31a051d10670f17f1ea32ac01
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-02-01 14:13:07 +0000

    App.js component complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'bulma/css/bulma.min.css';
 import Header from './components/Header'
-import EventDetails from './components/EventDetails';
+import EventSignUpForm from './components/EventSignUpForm';
+import EventSignUpList from './components/EventSignUpList';
 
 const App = () => {
+
+  const [userSignedIn, setUserSignedIn] = useState(false);
+
   return (
     <div>
-      <Header />
-      <EventDetails />
+      <Header
+        isSignedIn={userSignedIn}
+        handleSignInClick={() => setUserSignedIn(true)}
+        handleSignOutClick={()=> setUserSignedIn(false)}
+      />
+
+      <div className = 'section'>
+        <div className = 'container'>
+          <h1 className = 'title is-size-1'>Welcome to the Event sign up app</h1>
+          {
+            userSignedIn && <EventSignUpList />
+          }
+          {
+            !userSignedIn && <EventSignUpForm />
+          }
+        </div>
+      </div>
     </div>
   )
 }
