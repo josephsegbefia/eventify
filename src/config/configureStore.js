56345cbbf3ba6cdb2799af054fb3e5b9f5423f9d
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-02-03 10:08:16 +0000

    Edit configureStore.js

--- src/config/configureStore.js
+++ src/config/configureStore.js
@@ -1,17 +1,8 @@
 /* eslint-disable no-unused-vars */
-import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
-import { thunk }  from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 
 import rootReducer from '../reducers/reducers'
-import initialState from '../data/initialState';
 
-
-const configureStore = () => {
-  const middleWareEnhancer = applyMiddleware(thunk);
-  const composedEnhancers = compose(middleWareEnhancer);
-
-  const store = createStore(rootReducer, initialState, composedEnhancers);
-  return store;
-};
-
-export default configureStore;
+export default configureStore({
+  reducer: rootReducer,
+})
